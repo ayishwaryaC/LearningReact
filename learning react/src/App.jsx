747008fb886629967d1d27af14becc05103f7885
Ayishwarya C <124068540+ayishwaryaC@users.noreply.github.com>
2025-09-19 21:22:38 +0530

    Update App.jsx

--- learning react/src/App.jsx
+++ learning react/src/App.jsx
@@ -1,16 +1,22 @@
 
 import './App.css'
 // import Course from './course'
-import html from './assets/download.png'
+// import html from './assets/download.png'
 // import css3 from './assets/css3.png';
 // import Props from './props';
 // import js from './assets/js.png';
 // import Destructingprops from './destructingprops';
 
 // import Multipleprops from './multipleprops';
-import Defaultprops from './defaultprops';
-
-
+// import Defaultprops from './defaultprops';
+// import Conditionrendering1 from './conditionrendering1';
+// import Conditionrendering2 from './conditionrendering2';
+// import PropTypes from './proptypes'
+// import Dynamiccontent1 from './dynamiccontent1';
+// import Dynamiccontent2 from './dynamiccontent2';
+// import Dynamiccontent3 from './dynamiccontent3';\
+// import Dynamiccontent4 from './dynamiccontent4';
+import Keyinreact from './key1';
 function App(){
 
   return (
@@ -41,12 +47,50 @@ function App(){
 
     
 
-    <Defaultprops coursename="HTML5" para="Welcome to HTML5 Course" image={html}/>
+    {/* <Defaultprops coursename="HTML5" para="Welcome to HTML5 Course" image={html}/>
     <Defaultprops />                                                   
-    <Defaultprops />                                                      
-  
-    
+    <Defaultprops />  */}
+
+
+     {/* conditionrendering1 
+     <Conditionrendering1  coursename="HTML5" para="Welcome to HTML5 Course" image={html} show={true}/>
+     <Conditionrendering1  coursename="Css3"  para="Welcome to CSS3 Course"   image={css3} />                                                     
+     <Conditionrendering1  coursename="JS" para="Welcome to Javascript Course" image={js} show={true}/> */}
+
+
+
+
+    {/* Proptypes */}
+    {/* <PropTypes  coursename="HTML5" para="Welcome to HTML5 Course" image={html} show={true}/>
+     <PropTypes coursename="Css3"  para="Welcome to CSS3 Course"   image={css3} show={true}/>                                                     
+     <PropTypes coursename="JS" para="Welcome to Javascript Course" image={js} show={true}/> */}
+
+
+
+
+     {/* dynamic content using props */}
+     {/* <Dynamiccontent1  coursename="HTML5" para="Welcome to HTML5 Course" image={html} show={true}/>
+     <Dynamiccontent1 coursename="Css3"  para="Welcome to CSS3 Course"   image={css3} show={true}/>                                                     
+     <Dynamiccontent1 coursename="JS" para="Welcome to Javascript Course" image={js} show={true}/> */}
+
+
+
+     {/* dynamic content using conditional rendering */}
+     {/* <Dynamiccontent2  coursename="HTML5" para="Welcome to HTML5 Course" image={html} show={true}/>
+     <Dynamiccontent2 coursename="Css3"  para="Welcome to CSS3 Course"   image={css3} show={false}/>                                                     
+     <Dynamiccontent2 coursename="JS" para="Welcome to Javascript Course" image={js} show={true}/> */}
+
      
+
+      {/* dynamic content using lopp with .map() */}
+
+     {/* <Dynamiccontent3/> */}
+
+     {/* <Dynamiccontent4/> */}
+
+     <Keyinreact/>
+
+
     </>
   );
 }
